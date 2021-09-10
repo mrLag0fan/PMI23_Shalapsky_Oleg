8d2044691ad4c3f4ab85c03d1adcbccc3eab579c
Oleg Shalapsky <shalapsky.oleg@gmail.com>
2021-09-10 21:58:58 +0300

    better  performance

--- NP1_v1/main.py
+++ NP1_v1/main.py
@@ -1,37 +1,18 @@
-def print_arr(arr):
-    for i in range(len(arr)):
-        print(arr[i], end=' ')
-    print()
 
-def gener_bayte(arr, n):
-    arr.append("0")
-    arr.append("1")
-    if n == 1:
-        return
-    i = 0
-    while i < 2**n:
-        if(i >= len(arr)):
-            i = 0
-        temp = arr[i]
-        arr[i] += "0"
-        arr.insert(i+1, temp + "1")
-        i += 2
-
-def get_n():
-    print("Enter N:")
-    n = int(input())
+def enter_n():
+    try:
+        n = int(input("Enter your n:"))
+    except ValueError:
+        print("Your n isn't valid")
+        return enter_n()
     return n
 
-def search_11(arr):
+def serch(n):
     count = 0
-    for i in range(len(arr)):
-        if arr[i].find("11") != -1:
+    for i in range(2**n):
+        if str(bin(i)).find("11") == -1:
             count += 1
-    return count
+    print(f"Numbers with contains 11: {count}")
 
-arr = []
-n = get_n()
-gener_bayte(arr, n)
-print_arr(arr)
-count = search_11(arr)
-print(count)
+n = enter_n()
+serch(n)
